c03685c93d8cb2d3d53e49b78b5de480a917b1c3
eduardomaximo <eduardo.santosmaximo@gmail.com>
2022-04-15 12:45:04 -0300

    feature: stripe api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetStaticProps } from "next";
+
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 
+import { stripe } from "../services/stripe";
+
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,10 +29,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product?.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
@@ -29,3 +40,20 @@ export default function Home() {
     </>
   );
 }
+
+//executado no servidor do next, não no cliente
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KoqXELwQcHmBJu69Prwvhil");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 24, //24 hours
+  };
+};
