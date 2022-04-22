20feba19df7b0474400709e6d156d7bcbb6b42c3
eduardomaximo <eduardo.santosmaximo@gmail.com>
2022-04-22 09:16:31 -0300

    feature: create post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
-import styles from "./styles.module.scss";
+import Link from "next/link";
+
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
+
+import styles from "./styles.module.scss";
 
 type Post = {
   slug: string;
@@ -26,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a href="#" key={post.slug}>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
