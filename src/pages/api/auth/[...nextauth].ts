df56092f9b5ddbe24f8419faba027bc868a17874
eduardomaximo <eduardo.santosmaximo@gmail.com>
2022-04-22 11:16:16 -0300

    feature: validate subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,6 +19,33 @@ export default NextAuth({
   ],
   secret: process.env.JWT_SIGN_IN_KEY,
   callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch (err) {
+        console.log(err.message);
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn({ user }) {
       const { email } = user;
 
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,11 +41,17 @@ export const getServerSideProps: GetServerSideProps = async ({
   req,
   params,
 }) => {
-  const session = getSession({ req });
+  const session = await getSession({ req });
   const { slug } = params;
 
-  //   if (!session) {
-  //   }
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
   const prismic = getPrismicClient(req);
 
